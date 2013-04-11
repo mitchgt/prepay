577de754928f48a1a7286049eb790dac4c7fe7b5
larabear <lara.lu211@gmail.com>
2013-04-10 22:55:56 -0300

    added logout url

--- prepay/urls.py
+++ prepay/urls.py
@@ -6,17 +6,15 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^$', views.browse_listings, name='index'),
+    #url(r'^$', views.browse_listings, name='index'),
     url(r'^about$', views.about, name='about'),
     url(r'^category/(?P<category_id>.*)$', views.browse_category, name='browse_category'),
-    url(r'^browse_listings/search/$', views.browse_listings, name='search'),
-    url(r'^browse_listings/$', views.browse_listings, name='browse_listings'),
-#    url(r'^browse_product_requests$', views.browse_product_requests, name='browse_product_requests'),
+    url(r'^browse_listings$', views.browse_listings, name='browse_listings'),
+    url(r'^browse_product_requests$', views.browse_product_requests, name='browse_product_requests'),
     url(r'^register$', views.register, name = 'register'), ###Jennifer
-    url(r'^edit/(?P<user_username>.*)$', views.edit_profile, name = 'edit_profile'), 
-#    url(r'^user/(?P<user_username>.*)$', views.profile, name = 'user'),  ####Jennifer new
-    url(r'^profile/(?P<user_username>.*)$', views.profile, name = 'profile'), 
+    url(r'^user/(?P<user_username>.*)$', views.profile, name = 'profile'),  ####Jennifer new
     url(r'^listings/(?P<listing_id>.*)$', views.listing_detail, name='listing_detail'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='auth_logout'),
 )
