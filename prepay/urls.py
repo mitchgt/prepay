3efd80c860ed0f166520fd87805e2424daa0d12d
jlin45 <mmkkna@googlemail.com>
2013-04-12 00:14:13 -0300

    Update urls.py
    Uncommented edit and profile, commented out user

--- prepay/urls.py
+++ prepay/urls.py
@@ -13,9 +13,9 @@ urlpatterns = patterns('',
     url(r'^browse_listings/$', views.browse_listings, name='browse_listings'),
     url(r'^browse_product_requests$', views.browse_product_requests, name='browse_product_requests'),
     url(r'^register$', views.register, name = 'register'), ###Jennifer
-#    url(r'^edit/(?P<user_username>.*)$', views.edit_profile, name = 'edit_profile'), 
-    url(r'^user/(?P<user_username>.*)$', views.profile, name = 'user'),  ####Jennifer new
-#    url(r'^profile/(?P<user_username>.*)$', views.profile, name = 'profile'), 
+    url(r'^edit/(?P<user_username>.*)$', views.edit_profile, name = 'edit_profile'), 
+#    url(r'^user/(?P<user_username>.*)$', views.profile, name = 'user'),  ####Jennifer new
+    url(r'^profile/(?P<user_username>.*)$', views.profile, name = 'profile'), 
     url(r'^listings/(?P<listing_id>.*)$', views.listing_detail, name='listing_detail'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
