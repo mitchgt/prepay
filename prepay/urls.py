828d95e85d73f33ccc7b772558bf85c2d670e1a7
Jennifer Lin <jlin45@emory.edu>
2013-05-05 15:13:59 -0400

    Updated urls.py - hopefully logout redirects correctly

--- prepay/urls.py
+++ prepay/urls.py
@@ -7,7 +7,7 @@ admin.autodiscover()
 
 startup.set_groups()
 startup.create_default_users()
-startup.create_default_listing()
+#startup.create_default_listing()
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
@@ -40,7 +40,7 @@ urlpatterns = patterns('',
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': "/"}, name="auth_logout"),
     #url(r'^logout/$', 'django.contrib.auth.views.logout', views.index, name="auth_logout"),
-    url(r'^logout/(?P<next_page>.*)/$', views.index, name='auth_logout_next'),
+    url(r'^logout/(?P<next_page>.*)/$', views.logout, name='auth_logout_next'),
 
 
     url(r'^confirmation_code_sent/(?P<user_username>.*)$', views.confirmation_code_sent, name='confirmation_code_sent'),
